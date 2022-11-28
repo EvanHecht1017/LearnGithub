5af65a812050a1a2ce184383e0ecb1dfe613ad4e
evanhecht1017 <ehgames10@gmail.com>
2022-11-28 18:19:50 -0500

    new

--- apple.cpp
+++ apple.cpp
@@ -1,7 +1,25 @@
-// This is a file that is now being shown as a merrge
+/* rand example: guess the number */
+#include <stdio.h>      /* printf, scanf, puts, NULL */
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
 
+int main ()
+{
+  int iSecret, iGuess;
 
+  /* initialize random seed: */
+  srand (time(NULL));
 
-// Adding more lines
+  /* generate secret number between 1 and 10: */
+  iSecret = rand() % 10 + 1;
 
-// Another one
+  do {
+    printf ("Guess the number (1 to 10): ");
+    scanf ("%d",&iGuess);
+    if (iSecret<iGuess) puts ("The secret number is lower");
+    else if (iSecret>iGuess) puts ("The secret number is higher");
+  } while (iSecret!=iGuess);
+
+  puts ("Congratulations!");
+  return 0;
+}
